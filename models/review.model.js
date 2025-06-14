c55edc1b9fd8641e2af38eddaba38bc5a7a5b2e2
afienzidan <afienzidan227@gmail.com>
2025-06-14 16:26:09 +0700

    refactor(model): enable timestamps and validation for Review schema

--- models/review.model.js
+++ models/review.model.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  game_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  description: { type: String, required: true },
-  created_at: { type: Date, default: Date.now },
-});
+const reviewSchema = new mongoose.Schema(
+  {
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    game_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    description: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Review', reviewSchema);
+module.exports = mongoose.model("Review", reviewSchema);
