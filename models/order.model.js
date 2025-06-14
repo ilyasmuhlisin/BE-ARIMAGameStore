615f3cdfd8a70f74a6ecc73e9c5179b436144bb1
afienzidan <afienzidan227@gmail.com>
2025-06-14 22:36:41 +0700

    fix: change orders to array instead of object

--- models/order.model.js
+++ models/order.model.js
@@ -2,14 +2,13 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-    game_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }],
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    price: { type: Number, required: true },
+    total_price: { type: Number, required: true },
     payment_method: { type: String, enum: ["OVO", "BCA", "DANA", "ShopeePay", "QRIS"], required: true },
     proof: { type: String },
     status: { type: String, enum: ["pending", "completed"], default: "pending" },
   },
   { timestamps: true }
 );
-
 module.exports = mongoose.model("Order", orderSchema);
