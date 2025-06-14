b7724325c6f908bc69dde5b145a1f3111d2fd284
afienzidan <afienzidan227@gmail.com>
2025-06-14 16:26:04 +0700

    refactor(model): add timestamps to Developer schema

--- models/developer.model.js
+++ models/developer.model.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const developerSchema = new mongoose.Schema({
-  developer_name: { type: String, required: true },
-  description: String,
-  created_at: { type: Date, default: Date.now },
-});
+const developerSchema = new mongoose.Schema(
+  {
+    developer_name: { type: String, required: true },
+    description: String,
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Developer', developerSchema);
+module.exports = mongoose.model("Developer", developerSchema);
