3ca78689f6927daac61d06eeb3a290cf884aaf7c
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2025-06-23 11:39:32 +0700

    fix: update swagger docs

--- api/index.js
+++ api/index.js
@@ -57,7 +57,7 @@ const swaggerSpec = swaggerJSDoc({
     },
     security: [{ bearerAuth: [] }],
   },
-  apis: [path.join(__dirname, "../routes/*.js")], // ✅ path diperbaiki
+  apis: [path.join(__dirname, "routes/*.js")],
 });
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
